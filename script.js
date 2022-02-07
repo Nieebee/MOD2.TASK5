ad09fafe809af663d04c676add7e1bbbee36d5cf
Niédja Bezerra <niedjacarlos@gmail.com>
2022-02-06 21:05:57 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+ // Crie um array que receba 5 itens e exiba no console.
+let languages = ["English", "German", "Portuguese", "Spanish", "French"];
+console.log(languages);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+let languages = ["English", "German", "Portuguese", "Spanish", "French"];
+items.unshift("Italian");
+​console.log(items)
+
+// Utilize um método para remover o último nome do array.
+let languages = ["English", "German", "Portuguese", "Spanish", "French"];
+​items.pop("French");
+​console.log(items)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+let items = ["English", "German", "Portuguese", "Spanish", "French"];
+items.push("Turkish", "Japanese");
+console.log(items)
+
+// Utilize um método para remover o primeiro nome do array.
+let items = ["English", "German", "Portuguese", "Spanish", "French"];
+items.shift();
+console.log(items)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+let numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort();
+console.log(numbers);
